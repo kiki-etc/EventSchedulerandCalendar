52cacc24bc48119223bc6d3d6524e4615c4f933e
kiki-etc <kiki.etc@icloud.com>
2024-07-31 16:47:35 +0000

    Update Event.java
    improving accessor methods and overridden methods

--- Event.java
+++ Event.java
@@ -1,40 +1,49 @@
 import java.time.LocalDateTime;
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
 import java.util.Objects;
 
 public class Event implements Comparable<Event> {
+    private String title, venue, organization;
     private LocalDateTime dateTime;
-    private String title;
-    //private String ;
+    private boolean highPriority;
 
     public Event(LocalDateTime dateTime, String title) {
         this.dateTime = dateTime;
         this.title = title;
     }
 
-    public LocalDateTime getDateTime() {
-        return dateTime;
-    }
-
-    public void setDateTime(LocalDateTime dateTime) {
-        this.dateTime = dateTime;
-    }
-
+    // accessor methods
     public String getTitle() {
         return title;
     }
+    public LocalDateTime getDateTime() {
+        return dateTime;
+    }
+    public String getVenue() {
+        return venue;
+    }
+    public String getOrganization() {
+        return organization;
+    }
+    public boolean isHighPriority() {
+        return highPriority;
+    }
 
+    // mutator methods
+    public void setDateTime(LocalDateTime dateTime) {
+        this.dateTime = dateTime;
+    }
     public void setTitle(String title) {
         this.title = title;
     }
+    public void togglePriority() {
+        highPriority = !highPriority;
+    }
 
+    // overridden methods
     @Override
     public int compareTo(Event other) {
         return this.dateTime.compareTo(other.dateTime);
     }
-
     @Override
     public String toString() {
         return "Event{" +
@@ -42,16 +51,15 @@ public class Event implements Comparable<Event> {
                 ", name='" + title + '\'' +
                 '}';
     }
-
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
         Event event = (Event) o;
         return Objects.equals(dateTime, event.dateTime) &&
-                Objects.equals(title, event.title);
+               title.equals(event.title)                &&
+               organization.equals(event.organization);
     }
-
     @Override   // for working with collections like HashMaps etc.
     public int hashCode() {
         return Objects.hash(dateTime, title);
